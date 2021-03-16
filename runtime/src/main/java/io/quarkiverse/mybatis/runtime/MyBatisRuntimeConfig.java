78e2b9029d74f4e5c8fd457596acac4401f569da
Ihor Dmitriiev <i.dmitriiev@spd-ukraine.com>
2021-03-16 22:12:32 +0200

    Add all Configuration properties

--- runtime/src/main/java/io/quarkiverse/mybatis/runtime/MyBatisRuntimeConfig.java
+++ runtime/src/main/java/io/quarkiverse/mybatis/runtime/MyBatisRuntimeConfig.java
@@ -1,6 +1,14 @@
 package io.quarkiverse.mybatis.runtime;
 
 import java.util.Optional;
+import java.util.Set;
+
+import org.apache.ibatis.mapping.ResultSetType;
+import org.apache.ibatis.session.AutoMappingBehavior;
+import org.apache.ibatis.session.AutoMappingUnknownColumnBehavior;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.LocalCacheScope;
+import org.apache.ibatis.type.JdbcType;
 
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigRoot;
@@ -36,6 +44,179 @@ public class MyBatisRuntimeConfig {
      * MyBatis mapUnderscoreToCamelCase
      */
     @ConfigItem(defaultValue = "false")
-    public String mapUnderscoreToCamelCase;
+    public boolean mapUnderscoreToCamelCase;
 
+    /**
+     * MyBatis cacheEnabled
+     */
+    @ConfigItem(defaultValue = "true")
+    public boolean cacheEnabled;
+
+    /**
+     * MyBatis lazyLoadingEnabled
+     */
+    @ConfigItem(defaultValue = "false")
+    public boolean lazyLoadingEnabled;
+
+    /**
+     * MyBatis aggressiveLazyLoading
+     */
+    @ConfigItem(defaultValue = "false")
+    public boolean aggressiveLazyLoading;
+
+    /**
+     * MyBatis useColumnLabel
+     */
+    @ConfigItem(defaultValue = "true")
+    public boolean useColumnLabel;
+
+    /**
+     * MyBatis useGeneratedKeys
+     */
+    @ConfigItem(defaultValue = "false")
+    public boolean useGeneratedKeys;
+
+    /**
+     * MyBatis autoMappingBehavior
+     */
+    @ConfigItem(defaultValue = "PARTIAL")
+    public AutoMappingBehavior autoMappingBehavior;
+
+    /**
+     * MyBatis autoMappingUnknownColumnBehavior
+     */
+    @ConfigItem(defaultValue = "NONE")
+    public AutoMappingUnknownColumnBehavior autoMappingUnknownColumnBehavior;
+
+    /**
+     * MyBatis defaultExecutorType
+     */
+    @ConfigItem(defaultValue = "SIMPLE")
+    public ExecutorType defaultExecutorType;
+
+    /**
+     * MyBatis defaultStatementTimeout
+     */
+    @ConfigItem
+    public Optional<Integer> defaultStatementTimeout;
+
+    /**
+     * MyBatis defaultFetchSize
+     */
+    @ConfigItem
+    public Optional<Integer> defaultFetchSize;
+
+    /**
+     * MyBatis defaultResultSetType
+     */
+    @ConfigItem
+    public Optional<ResultSetType> defaultResultSetType;
+
+    /**
+     * MyBatis safeRowBoundsEnabled
+     */
+    @ConfigItem(defaultValue = "false")
+    public boolean safeRowBoundsEnabled;
+
+    /**
+     * MyBatis safeResultHandlerEnabled
+     */
+    @ConfigItem(defaultValue = "true")
+    public boolean safeResultHandlerEnabled;
+
+    /**
+     * MyBatis multipleResultSetsEnabled
+     */
+    @ConfigItem(defaultValue = "true")
+    public boolean multipleResultSetsEnabled;
+
+    /**
+     * MyBatis localCacheScope
+     */
+    @ConfigItem(defaultValue = "SESSION")
+    public LocalCacheScope localCacheScope;
+
+    /**
+     * MyBatis jdbcTypeForNull
+     */
+    @ConfigItem(defaultValue = "OTHER")
+    public JdbcType jdbcTypeForNull;
+
+    /**
+     * MyBatis lazyLoadTriggerMethods
+     */
+    @ConfigItem(defaultValue = "equals,clone,hashCode,toString")
+    public Set<String> lazyLoadTriggerMethods;
+
+    /**
+     * MyBatis defaultScriptingLanguage
+     */
+    @ConfigItem(defaultValue = "org.apache.ibatis.scripting.xmltags.XMLLanguageDriver")
+    public String defaultScriptingLanguage;
+
+    /**
+     * MyBatis defaultEnumTypeHandler
+     */
+    @ConfigItem(defaultValue = "org.apache.ibatis.type.EnumTypeHandler")
+    public String defaultEnumTypeHandler;
+
+    /**
+     * MyBatis callSettersOnNulls
+     */
+    @ConfigItem(defaultValue = "false")
+    public boolean callSettersOnNulls;
+
+    /**
+     * MyBatis returnInstanceForEmptyRow
+     */
+    @ConfigItem(defaultValue = "false")
+    public boolean returnInstanceForEmptyRow;
+
+    /**
+     * MyBatis logPrefix
+     */
+    @ConfigItem
+    public Optional<String> logPrefix;
+
+    /**
+     * MyBatis logImpl
+     */
+    @ConfigItem
+    public Optional<String> logImpl;
+
+    /**
+     * MyBatis proxyFactory
+     */
+    @ConfigItem(defaultValue = "JAVASSIST")
+    public String proxyFactory;
+
+    /**
+     * MyBatis vfsImpl
+     */
+    @ConfigItem
+    public Optional<String> vfsImpl;
+
+    /**
+     * MyBatis useActualParamName
+     */
+    @ConfigItem(defaultValue = "true")
+    public boolean useActualParamName;
+
+    /**
+     * MyBatis configurationFactory
+     */
+    @ConfigItem
+    public Optional<String> configurationFactory;
+
+    /**
+     * MyBatis shrinkWhitespacesInSql
+     */
+    @ConfigItem(defaultValue = "false")
+    public boolean shrinkWhitespacesInSql;
+
+    /**
+     * MyBatis defaultSqlProviderType
+     */
+    @ConfigItem
+    public Optional<String> defaultSqlProviderType;
 }
