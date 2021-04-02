c92427a1ca43f351ec6e9f7becfa0ef675d3d3a8
Ken Brumer <ken@brumer.org>
2021-04-01 23:34:36 -0400

    [#62] Add a integration test for MappedTypes and MappedJdbcTypes and test in native mode
    - [x] done

--- integration-tests/src/main/java/io/quarkiverse/it/mybatis/User.java
+++ integration-tests/src/main/java/io/quarkiverse/it/mybatis/User.java
@@ -1,10 +1,12 @@
 package io.quarkiverse.it.mybatis;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 public class User implements Serializable {
     private Integer id;
     private String name;
+    private UUID externalId;
 
     public Integer getId() {
         return id;
@@ -20,5 +22,13 @@ public class User implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public UUID getExternalId() {
+        return externalId;
+    }
+
+    public void setExternalId(UUID externalId) {
+        this.externalId = externalId;
     }
 }
