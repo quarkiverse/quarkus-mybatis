bb6ff225c79e8c2eaac925a11d60593915140902
Zohar <hunji66@gmail.com>
2021-09-18 22:36:22 +0800

    add some unit test for mybatis plus (#128)
    * add some unit test for mybatis plus
    
    * add reflectiveClass for sql wrapper
    
    * format code
    
    * add index dependency

--- mybatis-plus/deployment/src/main/java/io/quarkiverse/mybatis/plus/deployment/MyBatisPlusProcessor.java
+++ mybatis-plus/deployment/src/main/java/io/quarkiverse/mybatis/plus/deployment/MyBatisPlusProcessor.java
@@ -1,22 +1,36 @@
 package io.quarkiverse.mybatis.plus.deployment;
 
+import org.apache.ibatis.cache.decorators.LruCache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.AnnotationTarget;
+import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
+import org.jboss.logging.Logger;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import io.quarkiverse.mybatis.deployment.ConfigurationFactoryBuildItem;
 import io.quarkiverse.mybatis.deployment.SqlSessionFactoryBuilderBuildItem;
 import io.quarkiverse.mybatis.plus.runtime.MyBatisPlusConfigurationFactory;
 import io.quarkiverse.mybatis.runtime.meta.MapperDataSource;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 public class MyBatisPlusProcessor {
 
     private static final String FEATURE = "mybatis-plus";
     private static final DotName MYBATIS_PLUS_MAPPER = DotName.createSimple(BaseMapper.class.getName());
     private static final DotName MYBATIS_MAPPER_DATA_SOURCE = DotName.createSimple(MapperDataSource.class.getName());
+    private static final DotName MYBATIS_PLUS_WRAPPER = DotName.createSimple(Wrapper.class.getName());
+    private static final Logger LOG = Logger.getLogger(MyBatisPlusProcessor.class);
 
     @BuildStep
     FeatureBuildItem feature() {
@@ -31,5 +45,27 @@ public class MyBatisPlusProcessor {
     @BuildStep
     SqlSessionFactoryBuilderBuildItem createSqlSessionFactoryBuilder() {
         return new SqlSessionFactoryBuilderBuildItem(new MybatisSqlSessionFactoryBuilder());
+    }
+
+    @BuildStep
+    void addDependencies(BuildProducer<IndexDependencyBuildItem> indexDependency) {
+        indexDependency.produce(new IndexDependencyBuildItem("com.baomidou", "mybatis-plus-core"));
+    }
+
+    @BuildStep
+    void reflectiveClasses(BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
+            CombinedIndexBuildItem indexBuildItem) {
+
+        reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, PerpetualCache.class, LruCache.class));
+        for (AnnotationInstance i : indexBuildItem.getIndex().getAnnotations(DotName.createSimple(TableName.class.getName()))) {
+            if (i.target().kind() == AnnotationTarget.Kind.CLASS) {
+                DotName dotName = i.target().asClass().name();
+                reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, dotName.toString()));
+            }
+        }
+        reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, MYBATIS_PLUS_WRAPPER.toString()));
+        for (ClassInfo classInfo : indexBuildItem.getIndex().getAllKnownSubclasses(MYBATIS_PLUS_WRAPPER)) {
+            reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, classInfo.name().toString()));
+        }
     }
 }
--- mybatis-plus/integration-tests/src/test/java/io/quarkiverse/it/mybatis/plus/MyBatisPlusTest.java
+++ mybatis-plus/integration-tests/src/test/java/io/quarkiverse/it/mybatis/plus/MyBatisPlusTest.java
@@ -2,6 +2,8 @@ package io.quarkiverse.it.mybatis.plus;
 
 import static org.hamcrest.core.Is.is;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -14,5 +16,17 @@ public class MyBatisPlusTest {
     public void test() {
         RestAssured.when().get("/mybatis/plus/user/1").then()
                 .body(is("{\"id\":1,\"name\":\"Test User1\",\"externalId\":\"ccb16b65-8924-4c3f-8c55-681d85a16e79\"}"));
+
+        RestAssured.given().param("id", "5")
+                .param("name", "New User")
+                .param("externalId", UUID.fromString("9b036c98-eb1d-4580-a8bb-1115d7a3cd44"))
+                .post("/mybatis/plus/user")
+                .then().body(is("1"));
+
+        RestAssured.when().delete("/mybatis/plus/user/3").then()
+                .body(is("1"));
+
+        RestAssured.when().get("/mybatis/plus/user/count/h2").then()
+                .body(is("3"));
     }
 }
