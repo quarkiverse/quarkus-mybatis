494fd8ed6dca64d356d7e40cc6284605d1babe16
zohar <hunji66@gmail.com>
2021-09-18 11:33:10 +0800

    add some unit test for mybatis plus

--- mybatis-plus/integration-tests/src/main/java/io/quarkiverse/it/mybatis/plus/MyBatisPlusResource.java
+++ mybatis-plus/integration-tests/src/main/java/io/quarkiverse/it/mybatis/plus/MyBatisPlusResource.java
@@ -1,14 +1,19 @@
 package io.quarkiverse.it.mybatis.plus;
 
+import java.util.UUID;
+
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/mybatis/plus")
 public class MyBatisPlusResource {
+    private Logger logger = LoggerFactory.getLogger(MyBatisPlusResource.class);
     @Inject
     UserMapper userMapper;
 
@@ -18,4 +23,32 @@ public class MyBatisPlusResource {
     public User getUser(@PathParam("id") Integer id) {
         return userMapper.selectById(id);
     }
+
+    @Path("/user")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Integer createUser(@FormParam("id") Integer id, @FormParam("name") String name,
+            @FormParam("externalId") UUID externalId) {
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setExternalId(externalId);
+        return userMapper.insert(user);
+    }
+
+    @Path("/user/{id}")
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public Integer removeUser(@PathParam("id") Integer id) {
+        return userMapper.deleteById(id);
+    }
+
+    @Path("/user/count/h2")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public int getUserCount() {
+        return userMapper.selectCount(new QueryWrapper<>(new User()));
+    }
+
 }
