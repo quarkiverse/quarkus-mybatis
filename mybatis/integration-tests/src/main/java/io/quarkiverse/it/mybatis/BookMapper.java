955b1bce50061fa2243cb157eb1e995a8459260c
Amos Feng <zfeng@redhat.com>
2022-05-13 16:16:16 +0800

    Fix #72 remove SerializedCacheSubstitution and make sure CacheNameSpace works (#192)

--- mybatis/integration-tests/src/main/java/io/quarkiverse/it/mybatis/BookMapper.java
+++ mybatis/integration-tests/src/main/java/io/quarkiverse/it/mybatis/BookMapper.java
@@ -1,5 +1,6 @@
 package io.quarkiverse.it.mybatis;
 
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.One;
 import org.apache.ibatis.annotations.Result;
@@ -7,6 +8,7 @@ import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
+@CacheNamespace
 public interface BookMapper {
     @Select("SELECT id, author_id, title from books where id = #{id}")
     @Results(value = {
