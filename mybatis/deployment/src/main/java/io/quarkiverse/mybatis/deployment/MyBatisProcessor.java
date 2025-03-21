a2f694d100207873436d3b3105ed0f99b5711ff0
edbafrem <Rene.Markvard@sos.eu>
2025-03-21 10:56:46 +0100

    Switch from Singleton to ApplicationScoped for synthetic beans - fix #217

--- mybatis/deployment/src/main/java/io/quarkiverse/mybatis/deployment/MyBatisProcessor.java
+++ mybatis/deployment/src/main/java/io/quarkiverse/mybatis/deployment/MyBatisProcessor.java
@@ -1,26 +1,27 @@
 package io.quarkiverse.mybatis.deployment;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
+import io.quarkiverse.mybatis.runtime.MyBatisConfigurationFactory;
+import io.quarkiverse.mybatis.runtime.MyBatisRecorder;
+import io.quarkiverse.mybatis.runtime.MyBatisXMLConfigDelegateBuilder;
+import io.quarkiverse.mybatis.runtime.config.MyBatisDataSourceRuntimeConfig;
+import io.quarkiverse.mybatis.runtime.config.MyBatisRuntimeConfig;
+import io.quarkiverse.mybatis.runtime.meta.MapperDataSource;
+import io.quarkus.agroal.spi.JdbcDataSourceBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeansRuntimeInitBuildItem;
+import io.quarkus.deployment.annotations.*;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageProxyDefinitionBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+import io.quarkus.runtime.configuration.ConfigurationException;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
-import jakarta.inject.Singleton;
-
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.ibatis.annotations.DeleteProvider;
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.ResultType;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.annotations.UpdateProvider;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.cache.decorators.LruCache;
 import org.apache.ibatis.cache.impl.PerpetualCache;
 import org.apache.ibatis.javassist.util.proxy.ProxyFactory;
@@ -37,28 +38,13 @@ import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.DotName;
 import org.jboss.logging.Logger;
 
-import io.quarkiverse.mybatis.runtime.MyBatisConfigurationFactory;
-import io.quarkiverse.mybatis.runtime.MyBatisRecorder;
-import io.quarkiverse.mybatis.runtime.MyBatisXMLConfigDelegateBuilder;
-import io.quarkiverse.mybatis.runtime.config.MyBatisDataSourceRuntimeConfig;
-import io.quarkiverse.mybatis.runtime.config.MyBatisRuntimeConfig;
-import io.quarkiverse.mybatis.runtime.meta.MapperDataSource;
-import io.quarkus.agroal.spi.JdbcDataSourceBuildItem;
-import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
-import io.quarkus.arc.deployment.SyntheticBeansRuntimeInitBuildItem;
-import io.quarkus.deployment.annotations.BuildProducer;
-import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.annotations.Consume;
-import io.quarkus.deployment.annotations.ExecutionTime;
-import io.quarkus.deployment.annotations.Overridable;
-import io.quarkus.deployment.annotations.Record;
-import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
-import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.NativeImageProxyDefinitionBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
-import io.quarkus.runtime.configuration.ConfigurationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 public class MyBatisProcessor {
@@ -295,7 +281,7 @@ public class MyBatisProcessor {
             }
             SyntheticBeanBuildItem.ExtendedBeanConfigurator configurator = SyntheticBeanBuildItem
                     .configure(i.getMapperName())
-                    .scope(Singleton.class)
+                    .scope(ApplicationScoped.class)
                     .setRuntimeInit()
                     .unremovable()
                     .supplier(recorder.MyBatisMapperSupplier(i.getMapperName().toString(),
@@ -305,7 +291,7 @@ public class MyBatisProcessor {
         for (MyBatisMappedTypeBuildItem i : myBatisMappedTypesBuildItems) {
             SyntheticBeanBuildItem.ExtendedBeanConfigurator configurator = SyntheticBeanBuildItem
                     .configure(i.getMappedTypeName())
-                    .scope(Singleton.class)
+                    .scope(ApplicationScoped.class)
                     .setRuntimeInit()
                     .unremovable()
                     .supplier(recorder.MyBatisMappedTypeSupplier(i.getMappedTypeName().toString(),
@@ -315,7 +301,7 @@ public class MyBatisProcessor {
         for (MyBatisMappedJdbcTypeBuildItem i : myBatisMappedJdbcTypesBuildItems) {
             SyntheticBeanBuildItem.ExtendedBeanConfigurator configurator = SyntheticBeanBuildItem
                     .configure(i.getMappedJdbcTypeName())
-                    .scope(Singleton.class)
+                    .scope(ApplicationScoped.class)
                     .setRuntimeInit()
                     .unremovable()
                     .supplier(recorder.MyBatisMappedJdbcTypeSupplier(i.getMappedJdbcTypeName().toString(),
@@ -332,7 +318,7 @@ public class MyBatisProcessor {
         sqlSessionFactoryBuildItems.forEach(sqlSessionFactoryBuildItem -> {
             SyntheticBeanBuildItem.ExtendedBeanConfigurator configurator = SyntheticBeanBuildItem
                     .configure(SqlSessionFactory.class)
-                    .scope(Singleton.class)
+                    .scope(ApplicationScoped.class)
                     .unremovable()
                     .supplier(recorder.MyBatisSqlSessionFactorySupplier(sqlSessionFactoryBuildItem.getSqlSessionFactory()));
             String dataSourceName = sqlSessionFactoryBuildItem.getDataSourceName();
