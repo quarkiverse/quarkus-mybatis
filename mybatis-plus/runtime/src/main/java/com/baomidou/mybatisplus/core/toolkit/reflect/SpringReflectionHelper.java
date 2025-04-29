08632078cd4a57321de6b5ff65657cd14fbbb556
DefNed <8519834+Fengxq2014@users.noreply.github.com>
2025-04-29 10:40:20 +0800

    Update mybatis-plus/runtime/src/main/java/com/baomidou/mybatisplus/core/toolkit/reflect/SpringReflectionHelper.java
    Co-authored-by: Zheng Feng <zfeng@redhat.com>

--- mybatis-plus/runtime/src/main/java/com/baomidou/mybatisplus/core/toolkit/reflect/SpringReflectionHelper.java
+++ mybatis-plus/runtime/src/main/java/com/baomidou/mybatisplus/core/toolkit/reflect/SpringReflectionHelper.java
@@ -3,6 +3,6 @@ package com.baomidou.mybatisplus.core.toolkit.reflect;
 public class SpringReflectionHelper {
 
     public static Class<?>[] resolveTypeArguments(Class<?> clazz, Class<?> genericIfc) {
-        throw new RuntimeException("Illegal Access is class in quarkus.");
+        throw new RuntimeException("Not Supported");
     }
 }
